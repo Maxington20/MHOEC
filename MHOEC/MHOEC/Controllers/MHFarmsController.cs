3b58cabe0dec91a87108db62cb52b87625db0814
Max Herrington <max.herrington@gmail.com>
2018-01-28 14:52:32 -0500

    Completed to the end of the Farm Controller requirements.

---
+++ MHOEC/MHOEC/Controllers/MHFarmsController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MHOEC.Models;
+
+namespace MHOEC.Controllers
+{
+    public class MHFarmsController : Controller
+    {
+        private readonly OECContext _context;
+
+        public MHFarmsController(OECContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MHFarms
+        public async Task<IActionResult> Index()
+        {
+            var oECContext = _context.Farm
+				.Include(f => f.ProvinceCodeNavigation)
+				.OrderBy(f=> f.Name);
+            return View(await oECContext.ToListAsync());
+        }
+
+        // GET: MHFarms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farm = await _context.Farm
+                .Include(f => f.ProvinceCodeNavigation)
+                .SingleOrDefaultAsync(m => m.FarmId == id);
+            if (farm == null)
+            {
+                return NotFound();
+            }
+
+            return View(farm);
+        }
+
+        // GET: MHFarms/Create
+        public IActionResult Create()
+        {
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode");
+            return View();
+        }
+
+        // POST: MHFarms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FarmId,Name,Address,Town,County,ProvinceCode,PostalCode,HomePhone,CellPhone,Email,Directions,DateJoined,LastContactDate")] Farm farm)
+        {
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					_context.Add(farm);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+				ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", farm.ProvinceCode);
+				TempData["Message"] = "Farm has been added";
+				return View(farm);
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError(string.Empty,ex.InnerException.Message);
+				return View(farm);
+			}
+        }
+
+        // GET: MHFarms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farm = await _context.Farm.SingleOrDefaultAsync(m => m.FarmId == id);
+            if (farm == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", farm.ProvinceCode);
+			ViewData["ProvinceName"] = new SelectList(_context.Province, "Name", "Name", farm.ProvinceCode);
+
+			return View(farm);
+        }
+
+        // POST: MHFarms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FarmId,Name,Address,Town,County,ProvinceCode,PostalCode,HomePhone,CellPhone,Email,Directions,DateJoined,LastContactDate")] Farm farm)
+        {
+            if (id != farm.FarmId)
+            {
+                return NotFound();
+            }
+
+			try
+			{
+
+				if (ModelState.IsValid)
+				{
+					try
+					{
+						_context.Update(farm);
+						await _context.SaveChangesAsync();
+					}
+					catch (DbUpdateConcurrencyException)
+					{
+						if (!FarmExists(farm.FarmId))
+						{
+							return NotFound();
+						}
+						else
+						{
+							throw;
+						}
+					}
+					return RedirectToAction(nameof(Index));
+				}
+				ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", farm.ProvinceCode);
+				ViewData["ProvinceName"] = new SelectList(_context.Province, "Name", "Name", farm.ProvinceCode);
+
+				TempData["Message"] = "Farm has been edited";
+
+				return View(farm);
+			}
+
+			catch (Exception ex)
+			{
+				ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+				return View(farm);
+			}
+
+        }
+
+        // GET: MHFarms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farm = await _context.Farm
+                .Include(f => f.ProvinceCodeNavigation)
+                .SingleOrDefaultAsync(m => m.FarmId == id);
+            if (farm == null)
+            {
+                return NotFound();
+            }
+
+            return View(farm);
+        }
+
+        // POST: MHFarms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+			try
+			{
+				var farm = await _context.Farm.SingleOrDefaultAsync(m => m.FarmId == id);
+				_context.Farm.Remove(farm);
+				await _context.SaveChangesAsync();
+				TempData["Message"] = "Farm has been deleted";
+				return RedirectToAction(nameof(Index));
+			}
+
+			catch (Exception ex)
+			{
+				TempData["Message"] = ex.InnerException.Message;
+				return RedirectToAction("Delete", "MHFarms");
+			}
+        }
+
+        private bool FarmExists(int id)
+        {
+            return _context.Farm.Any(e => e.FarmId == id);
+        }
+    }
+}
---
+++ MHOEC/MHOEC/Controllers/MHProvincesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MHOEC.Models;
+
+namespace MHOEC.Controllers
+{
+    public class MHProvincesController : Controller
+    {
+        private readonly OECContext _context;
+
+        public MHProvincesController(OECContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MHProvinces
+        public async Task<IActionResult> Index()
+        {
+            var oECContext = _context.Province.Include(p => p.CountryCodeNavigation);
+            return View(await oECContext.ToListAsync());
+        }
+
+        // GET: MHProvinces/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Province
+                .Include(p => p.CountryCodeNavigation)
+                .SingleOrDefaultAsync(m => m.ProvinceCode == id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return View(province);
+        }
+
+        // GET: MHProvinces/Create
+        public IActionResult Create()
+        {
+			ViewData["CountryCode"] = new SelectList(_context.Country, "CountryCode", "CountryCode");
+			ViewData["CountryName"] = new SelectList(_context.Country.OrderBy(c=> c.Name), "Name", "Name");
+            return View();
+        }
+
+        // POST: MHProvinces/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProvinceCode,Name,CountryCode")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(province);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+			ViewData["CountryCode"] = new SelectList(_context.Country, "CountryCode", "CountryCode", province.CountryCode);
+			ViewData["CountryName"] = new SelectList(_context.Country.OrderBy(c => c.Name),"Name","Name");
+
+			return View(province);
+        }
+
+        // GET: MHProvinces/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Province.SingleOrDefaultAsync(m => m.ProvinceCode == id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryCode"] = new SelectList(_context.Country, "CountryCode", "CountryCode", province.CountryCode);
+			ViewData["CountryName"] = new SelectList(_context.Country.OrderBy(c=> c.Name), "Name", "Name", province.CountryCode);
+
+			return View(province);
+        }
+
+        // POST: MHProvinces/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ProvinceCode,Name,CountryCode")] Province province)
+        {
+            if (id != province.ProvinceCode)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(province);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProvinceExists(province.ProvinceCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryCode"] = new SelectList(_context.Country, "CountryCode", "CountryCode", province.CountryCode);
+			ViewData["CountryName"] = new SelectList(_context.Country, "Name", "Name", province.CountryCode);
+
+			return View(province);
+        }
+
+        // GET: MHProvinces/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var province = await _context.Province
+                .Include(p => p.CountryCodeNavigation)
+                .SingleOrDefaultAsync(m => m.ProvinceCode == id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return View(province);
+        }
+
+        // POST: MHProvinces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var province = await _context.Province.SingleOrDefaultAsync(m => m.ProvinceCode == id);
+            _context.Province.Remove(province);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProvinceExists(string id)
+        {
+            return _context.Province.Any(e => e.ProvinceCode == id);
+        }
+    }
+}
